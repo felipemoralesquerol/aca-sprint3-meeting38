fd3aa00e2dfc78a90f50feaa52bac080c904f7f0
Felipe Morales <felipe.morales.querol@gmail.com>
2021-11-03 19:29:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Esta es información obtenidad desde tu API')
+})
+
+app.listen(3000, () => {
+    console.log('Aplicación escuchando en puerto 3000');
+})
